399329f043d6618f9cd17fa3f2a7f673da5cccd9
ch <52480721@qq.com>
2025-09-30 12:49:41 +0800

    init

---
+++ readme.txt
@@ -0,0 +1,3 @@
+从bootstrap的示例中拷贝
+https://github.com/twbs/examples/tree/main/webpack
+
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+'use strict'
+
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // 引入插件
+const loader = require('sass-loader')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer:{
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css'       // 输出的 CSS 文件路径和名称
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          // MiniCssExtractPlugin.loader,
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            // loader: 'style-loader'
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer
+                ]
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                // Silence Sass deprecation warnings
+                silenceDeprecations: [
+                  'mixed-decls',
+                  'color-functions',
+                  'global-builtin',
+                  'import'
+                ]
+              }
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
